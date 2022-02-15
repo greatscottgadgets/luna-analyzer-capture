7d390d501b4bbac7008f97b8246d325a1d28b7a6
Martin Ling <martin-git@earth.li>
2022-02-15 05:06:31 +0000

    Automatically import all symbols from cffi library.

--- interface.py
+++ interface.py
@@ -1,7 +1,11 @@
 from cffi import FFI
 
-def load_capture():
-    ffi = FFI()
-    ffi.cdef(open('library.h').read())
-    lib = ffi.dlopen('./library.so')
-    return lib.load_capture()
+ffi = FFI()
+ffi.cdef(open('library.h').read())
+lib = ffi.dlopen('./library.so')
+
+__all__ = dir(lib)
+
+g = globals()
+for name in __all__:
+    g[name] = getattr(lib, name)
