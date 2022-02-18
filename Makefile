82d18e08b862ce8961eda9f543847f1406c116cc
Martin Ling <martin-git@earth.li>
2022-02-18 03:59:12 +0000

    Add decode test program.

--- Makefile
+++ Makefile
@@ -2,12 +2,19 @@ DEPS = libusb-1.0 libpcap
 CFLAGS = -g -Wall $(shell pkg-config --cflags $(DEPS))
 LIBS = $(shell pkg-config --libs $(DEPS))
 
-all: capture luna2pcap library.so
+all: capture luna2pcap decode_test library.so
 
 library.so: library.h
+library.o: library.h
+
+decode_test: decode_test.o library.o
+	gcc $(CFLAGS) $^ -o $@
 
 %: %.c Makefile
 	gcc $(CFLAGS) $< $(LIBS) -o $@
 
 %.so: %.c Makefile
 	gcc -shared $(CFLAGS) $< $(LIBS) -o $@
+
+%.o: %.c Makefile
+	gcc -c $(CFLAGS) $< $(LIBS) -o $@
---
+++ decode_test.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "library.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		printf("Usage: %s <filename>\n", argv[0]);
+		return -1;
+	}
+
+	char *filename = argv[1];
+
+	struct capture *capture = convert_capture(filename);
+
+	printf("%s: %lu packets, %lu transactions, %lu endpoints, %lu transfers\n",
+		filename,
+		capture->num_packets,
+		capture->num_transactions,
+		capture->num_endpoints,
+		capture->num_transfers);
+
+	for (int i = 0; i < capture->num_endpoints; i++) {
+		struct endpoint *ep = &capture->endpoints[i];
+		struct endpoint_traffic *traf = capture->endpoint_traffic[i];
+		printf("%u.%u: %lu transfers, %lu transactions\n",
+			ep->address, ep->endpoint,
+			traf->num_transfers, traf->num_transaction_ids);
+	}
+
+	close_capture(capture);
+
+	return 0;
+}
